b3ff5be45bc8bbaf10f8febc3a1cf292a5ffcd1a
ChuckTG <ktzirvelakis@gmail.com>
2020-06-23 20:30:13 +0300

    added pooling layer

--- nn_conv.py
+++ nn_conv.py
@@ -68,3 +68,38 @@ def conv_forward(A_prev,W,b,hparams):
 
     cache = (A_prev,W,b,hparams)
     return Z,cache
+
+
+def pool_forward(A_prev, hparameters, mode="max"):
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+
+    n_H = int((n_H_prev-f)/stride) + 1
+    n_W = int((n_W_prev-f)/stride) + 1
+
+
+    A = torch.zeros((m, n_H, n_W, n_C_prev))
+
+    for i in range(m):
+        for h in range(n_H):
+            vert_start = h * stride
+            vert_end = vert_start + f
+            for w in range(n_W):
+                horiz_start = w * stride
+                horiz_end   = horiz_start +f
+                for c in range(n_C_prev):
+                    a_prev_slice = A_prev[i,vert_start:vert_end,horiz_start:horiz_end,c]
+
+                    if mode == "max":
+                        A[i,h,w,c] = a_prev_slice.max()
+                    elif mode =="average":
+                        A[i,h,w,c] = a_prev_slice.mean()
+
+    cache = (A_prev,hparameters)
+
+    assert(A.shape == (m,n_H,n_W,n_C_prev))
+
+    return A,cache
+
+
